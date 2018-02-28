6cd28a2557f31ad482eaae1e5b2d5bff90c2626d
Victor Pellerin <pellerin.victor@gmail.com>
2018-02-28 16:31:46 +0100

    setting up get routes for user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', (req, res, next) => {
+  User.find({}, (err, users) => {
+    if (err) { return res.json(err).status(500); }
+
+    return res.json(users);
+  });
 });
 
 module.exports = router;
