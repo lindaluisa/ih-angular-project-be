385dbf25279666c359da358cec8001de462730ef
Victor Pellerin <pellerin.victor@gmail.com>
2018-02-28 16:56:02 +0100

    minor changes indentation

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const User = require('../models/user');
 
 /* GET users listing. */
-
 router.get('/', (req, res, next) => {
   User.find({}, (err, users) => {
     if (err) { return res.json(err).status(500); }
