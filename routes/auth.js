ffbbc7f36f14bf15cba6c2f1bd11f1133437be34
Victor Pellerin <pellerin.victor@gmail.com>
2018-02-28 15:11:25 +0100

    set route for me

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,13 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-// router.get('/me', (req, res, next) => {
-//   if (req.session.currentUser) {
-//     res.json(req.session.currentUser);
-//   } else {
-//     res.status(404).json({error: 'not-found'});
-//   }
-// });
+router.get('/me', (req, res, next) => {
+  if (req.session.currentUser) {
+    res.json(req.session.currentUser);
+  } else {
+    res.status(404).json({error: 'not-found'});
+  }
+});
 
 router.post('/login', (req, res, next) => {
   if (req.session.currentUser) {
@@ -74,9 +74,9 @@ router.post('/signup', (req, res, next) => {
     .catch(next);
 });
 
-// router.post('/logout', (req, res) => {
-//   req.session.currentUser = null;
-//   return res.status(204).send();
-// });
+router.post('/logout', (req, res) => {
+  req.session.currentUser = null;
+  return res.status(204).send();
+});
 
 module.exports = router;
